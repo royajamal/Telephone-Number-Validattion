877d1b56f544bd06869b5f418698478520c7c55b
unknown <royajamal2003@gmail.com>
2024-07-04 03:10:30 -0700

    script.js edited

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-document.getElementById('check-btn').addEventListener('click', () => {
-  const userInput = document.getElementById('user-input').value.trim();
-  const resultsDiv = document.getElementById('results-div');
-  const validUSNumber = /^(1\s?)?(\(\d{3}\)|\d{3})([\s\-]?)\d{3}([\s\-]?)\d{4}$/;
+const userInput = document.getElementById('user-input');
+const checkBtn = document.getElementById('check-btn');
+const clearBtn = document.getElementById('clear-btn');
+const resultsDiv = document.getElementById('results-div');
 
-  if (!userInput) {
+function checkValidNumber(input) {
+  const countryCode = '^(1\\s?)?';
+  const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
+  const spacesDashes = '[\\s\\-]?';
+  const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
+  const phoneRegex = new RegExp(`${countryCode}${areaCode}${spacesDashes}${phoneNumber}`);
+  const isValid = phoneRegex.test(input);
+
+  resultsDiv.style.color = isValid ? '#066637' : '#af0f0f';
+  resultsDiv.innerHTML = `${isValid ? 'Valid' : 'Invalid'} US number: ${input}`;
+}
+
+checkBtn.addEventListener('click', () => {
+  const input = userInput.value;
+  if (input === '') {
     alert('Please provide a phone number');
-    return;
   }
-
-  if (validUSNumber.test(userInput)) {
-    resultsDiv.textContent = `Valid US number: ${userInput}`;
+  if (input.length === 0) {
+    resultsDiv.style.color = '#af0f0f';
+    resultsDiv.innerHTML = 'Please provide a phone number';
   } else {
-    resultsDiv.textContent = `Invalid US number: ${userInput}`;
+    checkValidNumber(input);
+    userInput.value = '';
   }
 });
 
-document.getElementById('clear-btn').addEventListener('click', () => {
-  document.getElementById('user-input').value = '';
-  document.getElementById('results-div').textContent = '';
+userInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    const input = userInput.value;
+    checkValidNumber(input);
+    userInput.value = '';
+  }
+});
+
+clearBtn.addEventListener('click', () => {
+  resultsDiv.innerHTML = '';
 });
