159130137814d93820e6200b2f9164113b188744
unknown <royajamal2003@gmail.com>
2024-07-03 23:35:47 -0700

    script.js file linter error solved

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-document.getElementById("check-btn").addEventListener("click", () => {
-  const userInput = document.getElementById("user-input").value.trim();
-  const resultsDiv = document.getElementById("results-div");
+document.getElementById('check-btn').addEventListener('click', () => {
+  const userInput = document.getElementById('user-input').value.trim();
+  const resultsDiv = document.getElementById('results-div');
   const validUSNumber = /^(1\s?)?(\(\d{3}\)|\d{3})([\s\-]?)\d{3}([\s\-]?)\d{4}$/;
 
   if (!userInput) {
-      alert("Please provide a phone number");
-      return;
+    alert('Please provide a phone number');
+    return;
   }
 
   if (validUSNumber.test(userInput)) {
-      resultsDiv.textContent = `Valid US number: ${userInput}`;
+    resultsDiv.textContent = `Valid US number: ${userInput}`;
   } else {
-      resultsDiv.textContent = `Invalid US number: ${userInput}`;
+    resultsDiv.textContent = `Invalid US number: ${userInput}`;
   }
 });
 
-document.getElementById("clear-btn").addEventListener("click", () => {
-  document.getElementById("user-input").value = "";
+document.getElementById('clear-btn').addEventListener('click', () => {
+  document.getElementById('user-input').value = '';
   document.getElementById('results-div').textContent = '';
 });
